ee2e779e9b230988750b373e1207421f941457f1
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2022-07-14 02:42:14 +0900

    Create lv01_basicString_kyj.c

---
+++ programmers_lv1/lv01_basicString_kyj.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h> 
+#include <stdlib.h>
+#include <stdbool.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+bool solution(const char* s) {
+    
+    int i;
+    
+    
+    for(i=0; i< strlen(s); i++){
+        if('0'< s[i] && '9'> s[i])
+            break;
+    }
+
+    if(i== strlen(s)) 
+        return true;
+    else if(i!= strlen(s)) 
+        return false;
+
+}
