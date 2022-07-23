eab746fccafbe4ba89a2cdd189ddd0a6f9e5c497
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2022-07-23 17:17:42 +0900

    Update lv01_MiddleChar_kyj.c

--- programmers_lv1/week4/lv01_MiddleChar_kyj.c
+++ programmers_lv1/week4/lv01_MiddleChar_kyj.c
@@ -1,3 +1,5 @@
+문제: https://school.programmers.co.kr/learn/courses/30/lessons/12903
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,18 +9,16 @@ char* solution(const char* s) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     char* answer;
     
-    int length= strlen(s);
+    int length= strlen(s); 
     
-    if(length%2) {
+    if(length%2) { 
         answer = (char *)malloc(sizeof(char) * 2);
         answer[0]= s[length/2];
-        answer[1]='\0';
     }
     else {
         answer = (char *)malloc(sizeof(char) * 3);
         answer[0] = s[length/2-1];
         answer[1] = s[length/2];
-        answer[2]='\0';
     }
     
     return answer;
