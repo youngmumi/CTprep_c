9c3ca73d190470d180d479adf4cd65c782214630
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2022-08-02 23:35:14 +0900

    Update lv01_Collatz_kyj.c

--- programmers_lv1/week4/lv01_Collatz_kyj.c
+++ programmers_lv1/week4/lv01_Collatz_kyj.c
@@ -7,16 +7,14 @@ int solution(long long num) { //must be 'long long'
      int answer=0;
     
     while(num!=1){
-    	if(num!=0){
-    		if(num%2==0) {
-            		num/=2;
-            		answer++;
-            	}
-        	else {
-            		num=num*3+1;
-            		answer++;
-        	}
-	}
+    	if(num%2==0) {
+            	num/=2;
+            	answer++;
+            }
+        else {
+            	num=num*3+1;
+            	answer++;
+        }
     }
    
     if(answer>=500) return -1 ;
