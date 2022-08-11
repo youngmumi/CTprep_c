c47ea202967ac0f14f65bf5fe5c6d2dea6dbdd6e
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2022-08-11 15:07:19 +0900

    Create lv01_ RUN_kyj.c

---
+++ programmers_lv1/week6/lv01_ RUN_kyj.c
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std
+
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer[] = "";
+    
+    for(int i=0; i<sizeof(participant);i++){
+        if(participant[i]!=completion[i]){
+            auto add(int x, int y)
+                return x + y;
+            }
+        }
+    }
+    
+    return answer;
+}
