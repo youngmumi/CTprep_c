bef86c7a6922d86fdfce685321139937917c0a57
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2022-08-18 06:15:56 +0900

    Update lv01_pickdoll_kyj.cpp

--- programmers_lv1/week7/lv01_pickdoll_kyj.cpp
+++ programmers_lv1/week7/lv01_pickdoll_kyj.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <vector>
-#include <stack>
+#include <stack> // To use stack
 
 using namespace std;
 
@@ -8,24 +8,26 @@ using namespace std;
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
     stack<int> s;
-    for(int i=0;i<moves.size();i++)
+    
+    for(int i=0;i<moves.size();i++) //Scan all 'moves'
     {
-        int check = moves[i] - 1;
-        for(int j=0;j<board.size();j++)
+        int check = moves[i] - 1; //-1 because it must be in an array
+        for(int j=0;j<board.size();j++) //Scan all 'board'
         {
-            if(board[j][check] != 0)
+            if(board[j][check] != 0) //Pass by when it's 0
             {
-                if(!s.empty() && s.top() == board[j][check])
+                if(!s.empty() && s.top() == board[j][check]) //If the stack is empty and on top of the stack is the same as the picked doll
                 {
-                    s.pop();
-                    answer += 2;
+                    s.pop(); //The one on the stack pop
+                    answer += 2; //Add 2 to answer
                 }
                 else
-                    s.push(board[j][check]);
-                board[j][check] = 0;
-                break;
+                    s.push(board[j][check]); //Pushing a Caught Doll onto a Stack
+                board[j][check] = 0; //Get rid of the picked doll
+                break; //break and Move to 14 lines
             }
         }
     }
+    
     return answer;
 }
