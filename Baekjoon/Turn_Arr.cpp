bb5b51034163231864ff461776d13375d99bed60
kim yu jeong <88278522+youngmumi@users.noreply.github.com>
2024-02-03 14:10:14 +0900

    Update Turn_Arr.cpp

--- Baekjoon/Turn_Arr.cpp
+++ Baekjoon/Turn_Arr.cpp
@@ -6,12 +6,49 @@ using namespace std;
 
 #define MAX 300+1
 
-int N, M;
-int R;
+int N, M; // 배열 크기 N*M
+int R; // 입력된 회전 수
 
 int A[MAX][MAX];
+int tmp[MAX][MAX];
 
-void input(){
+void rocate(){
+    int rocate_num = R % (N*2 + M*2 - 4);
+    int x=1, y=1;
+    int start = 1;
+    int last_i = N;
+    int last_j = M;
+    
+    for(i=1; i=<N; i++){
+        for(j=1; j=<M; j++){
+            x=1;
+            y=1;
+            for(int k=0; k<rocate_num; k++){
+                if(i=start && j!=last_j)
+                    y++;
+                if(j=last_j && i!=last_i)
+                    x++;
+                if(i=last_i && j!=start)
+                    y--;
+                if(j=start && i!= start)
+                    x--;
+            }
+            tmp[x][y] = A[i][j];
+        }
+    }
+    
+}
+
+void print(){
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=M; j++){
+            cout << tmp[i][j];
+        }
+    }
+}
+
+int main(){
+    
     cin >> N >> M >> R;
     
     for(int i=1; i<=N; i++){
@@ -19,21 +56,10 @@ void input(){
             cin >> A[i][j];
         }
     }
-}
-
-void print(){
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=M; j++){
-            cout << A[i][j];
-        }
-    }
-}
-
-void solve(){
-    input();
-    print();
-}
-
-int main(){
     
+    rocate();
+    
+    print();
+    
+    return 0;
 }
